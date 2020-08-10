a156f67c06b4be0e3b28281fba1cf7b01ce0b432
Jake Krayger <jkrayg@protonmail.com>
2020-08-10 14:42:46 -0400

    fixing heroku

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
     host: '/',
-    port: '3001'
+    port: '3001',
+    path: 'https://agile-shore-99216.herokuapp.com/'
 })
 
 const myVideo = document.createElement("video");
