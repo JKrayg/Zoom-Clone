56748ca2f2171aab056f819c93ccc490e80d3043
Jake Krayger <jkrayg@protonmail.com>
2020-08-10 14:50:22 -0400

    neww

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-    host: '/' || 'https://agile-shore-99216.herokuapp.com/',
-    port: '3001',
+    secure: true,
+    host: 'https://agile-shore-99216.herokuapp.com',
+    port: '3001'
 })
 
 const myVideo = document.createElement("video");
