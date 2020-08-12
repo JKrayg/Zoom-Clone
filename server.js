fca6680df892f121b2c8968e088f97d1d7825f9e
Jake Krayger <jkrayg@protonmail.com>
2020-08-12 19:28:29 -0400

    arsg

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const { v4: uuidV4 } = require("uuid")
 const PORT = process.env.PORT || 3000;
 
 if(PORT === process.env.PORT) {
-    const peerServer = PeerServer({
-        host: 'agile-shore-99216.herokuapp.com',
-        port: process.env.PORT || 9000,
-        ssl: {
-        key: fs.readFileSync('server.key', "utf8"),
-        cert: fs.readFileSync('server.csr', "utf8")
-        }
-    });
+    // const peerServer = PeerServer({
+    //     host: 'agile-shore-99216.herokuapp.com',
+    //     port: process.env.PORT || 9000,
+    //     ssl: {
+    //     key: fs.readFileSync('server.key', "utf8"),
+    //     cert: fs.readFileSync('server.csr', "utf8")
+    //     }
+    // });
+    const peerServer = PeerServer({ port: 9000, path: '/myapp' });
     app.use('/myapp', peerServer);
 }
 
