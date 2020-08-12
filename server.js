ece35c90dd72015c6b1763a652aeb468a0affa67
Jake Krayger <jkrayg@protonmail.com>
2020-08-12 18:26:55 -0400

    last

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ if(PORT === process.env.PORT) {
         host: 'agile-shore-99216.herokuapp.com',
         port: process.env.PORT || 9000,
         ssl: {
-        key: fs.readFileSync('server.key', {encoding: "base64"}),
+        key: fs.readFileSync('server.key', "utf8", function(error, data) {
+            console.log(data)
+        }),
         cert: fs.readFileSync('server.csr', {encoding: "base64"})
         }
     });
